7cf4a62dacdc8b2f1d7c083877e5b4b5d24e167f
KASTORIDES <49335900+KASTORIDES@users.noreply.github.com>
2025-10-24 02:12:31 +0300

    Add files via upload

---
+++ Student Management System/StudentSystem.Common/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StudentSystem.Common
+{
+    public class Course
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public Course(string title, int credits)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            Credits = credits;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} ({Credits} credits)";
+        }
+    }
+}
+

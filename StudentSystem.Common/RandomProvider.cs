48ccd77ca19ba8115d84dd7a2793af8f50713bca
KASTORIDES <49335900+KASTORIDES@users.noreply.github.com>
2025-12-01 01:14:02 +0200

    Add files via upload

---
+++ StudentSystem.Common/RandomProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace StudentSystem.Common
+{
+    // Безпечний генератор випадкових чисел для багатопотоковості
+    public static class RandomProvider
+    {
+        private static int _seed = Environment.TickCount;
+        private static readonly ThreadLocal<Random> _threadRandom = new ThreadLocal<Random>(() =>
+            new Random(Interlocked.Increment(ref _seed))
+        );
+
+        public static Random GetThreadRandom() => _threadRandom.Value;
+    }
+}
